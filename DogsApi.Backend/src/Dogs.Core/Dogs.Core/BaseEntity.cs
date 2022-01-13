da39778a5660bec4c45abb3431918607f5f74711
TkachukSasha <tkachuko_09@knu.ua>
2022-01-13 12:02:55 +0200

    Create models

---
+++ DogsApi.Backend/src/Dogs.Core/Dogs.Core/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dogs.Core
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ DogsApi.Backend/src/Dogs.Core/Dogs.Core/Entities/Dog.cs
@@ -0,0 +1,10 @@
+﻿namespace Dogs.Core.Entities
+{
+    public class Dog : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public float TailLenght { get; set; }
+        public float Weight { get; set; }
+    }
+}
